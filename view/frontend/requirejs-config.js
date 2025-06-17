bbbc11b0717e70fef61c340c3b357e8306fe8b3e
Justin Taylor <jt@justintaylor.org>
2025-06-17 10:23:14 +0100

    Put back

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -9,8 +9,7 @@ var config = {
 		'cravendunnill-cart-counter': 'CravenDunnill_Header/js/cart-counter',
 		'cravendunnill-minicart-delete': 'CravenDunnill_Header/js/minicart-delete',
 		'cravendunnill-add-to-cart-handler': 'CravenDunnill_Header/js/add-to-cart-handler',
-		'cravendunnill-add-to-cart-override': 'CravenDunnill_Header/js/add-to-cart-override',
-		'cravendunnill-tile-sample-cart-handler': 'CravenDunnill_TileSamples/js/tile-sample-cart-handler'
+		'cravendunnill-add-to-cart-override': 'CravenDunnill_Header/js/add-to-cart-override'
 	},
 	// Add this priority loading configuration
 	deps: ['cravendunnill-add-to-cart-override', 'cravendunnill-cart-counter'],
